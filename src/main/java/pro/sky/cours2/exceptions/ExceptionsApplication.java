9e6c632f463f5887fdc99f4ab677c8cab6ab477a
AnnaLihanova <anja.lihanova@gmail.com>
2022-02-23 19:48:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cours2/exceptions/ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cours2.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionsApplication.class, args);
+    }
+
+}
