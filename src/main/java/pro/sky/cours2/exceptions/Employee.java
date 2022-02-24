229dd5842bab535a278f9cd4bb275e1f34fc01a5
AnnaLihanova <anja.lihanova@gmail.com>
2022-02-24 20:47:45 +0300

    corrected

--- src/main/java/pro/sky/cours2/exceptions/Employee.java
+++ src/main/java/pro/sky/cours2/exceptions/Employee.java
@@ -1,5 +1,7 @@
 package pro.sky.cours2.exceptions;
 
+import java.util.Objects;
+
 public class Employee {
     private final String lastName;
     private final String firstName;
@@ -19,5 +21,18 @@ public class Employee {
 
     public String toString() {
         return getLastName() + " " + getFirstName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return lastName.equals(employee.lastName) && firstName.equals(employee.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, firstName);
     }
 }
